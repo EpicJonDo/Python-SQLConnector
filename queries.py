fbf7dd1309eeb02a3cf2e9eea61b9491e2c12230
Jondo <gabrielayala1507@gmail.com>
2026-08-20 22:27:20 -0500

    Basic cleaning, still need to work on display db

--- queries.py
+++ queries.py
@@ -4,35 +4,36 @@ import sqlite3
 # Specify path to directory holding the databases
 db_folder = Path("databases")
 
+# Global Variables
+db_list = [file.stem for file in db_folder.rglob("*.db")]
 
-# "Home" page
-def home():
+# "Home" page, prints databases from path
+# Then asks the user to choose one (or create a new ono)
+def homepage():
 	print("Hello and welcome to my program")
 	print("Choose a database to use by entering the number associated with it, or enter 0 to add a new database:")
-	db_list = [file.stem for file in db_folder.rglob("*.db")]
 
 	for index, db in enumerate(db_list, start=1):
 		print(f"({index}) {db}")
 
-	choose_db = int(input())
+	choose_db = int(input(">>"))
 
 	if choose_db == 0:
-		new_db = str(input(print("Enter a name for the database:")))
-		sqlite3.connect(db_folder + "/" + new_db + ".db")
-		print("Successfully created database")
-		home()
+		new_db = str(input("Enter a name for the database:"))
+		sqlite3.connect(str(db_folder) + "/" + new_db + ".db")
+		print("Successfully created database\n")
 	else:
-		choose_db -= 1
-		user_db = sqlite3.connect(str(db_folder) + "/" + db_list[choose_db] + ".db")
-		print("Opening " + db_list[choose_db])
+		user_db = sqlite3.connect(str(db_folder) + "/" + db_list[choose_db - 1] + ".db")
+		print("Opening " + db_list[choose_db - 1])
 	return choose_db
 
-def nextpage(use_db):
-	dblist = [file.stem for file in db_folder.rglob("*.db")]
-	use_db -=1
-	user_db = sqlite3.connect(str(db_folder) + "/" + dblist[use_db])
-	cursor = user_db.cursor
-	print("connected")
+# Prints all tables from Database
+def displaydb(use_db):
+	user_db = sqlite3.connect(str(db_folder) + "/" + db_list[use_db - 1] + ".db")
+	cursor = user_db.cursor()
+	cursor.execute("show tables")
+	print(cursor.fetchall)
+	print("done")
 
-nextpage(home())
+displaydb(homepage())
 print("DONE")
